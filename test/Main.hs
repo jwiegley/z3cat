a454cb96b778626b6a3712e102fc0925306240c8
John Wiegley <johnw@newartisans.com>
2017-04-18 16:35:13 -0700

    In the test, print out the rendered categorical term

--- test/Main.hs
+++ test/Main.hs
@@ -8,21 +8,29 @@ module Main where
 
 import ConCat.AltCat (ccc)
 import ConCat.Category hiding (it)
+import ConCat.Syntactic (render)
 import Prelude hiding ((.), id, curry, uncurry)
 import Test.Hspec
 import Z3.Category
 
 equation :: (Num a, Ord a) => a -> a -> Bool
 equation x y =
+    let f x y = x + y < 100 in
     x < y &&
     y < 100 &&
+    uncurry f (10, 20) &&
     0 <= x - 3 + 7 * y &&
     (x == y || y + 20 == x + 30)
 {-# INLINE equation #-}
 
-main :: IO ()
-main = hspec $
+spec :: IO ()
+spec = hspec $
     describe "Basic tests" $
         it "Runs a Haskell function through Z3" $
             runZ3 (ccc (uncurry (equation @Int)))
                 `shouldReturn` Just (-8, 2)
+
+main :: IO ()
+main = do
+    putStrLn $ render (ccc (uncurry (equation @Int)))
+    spec
